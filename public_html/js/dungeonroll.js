6e396cedc12ffef0c12369f2afffef0502c023c2
sitnaltax <sitnaltax@gmail.com>
2014-03-23 22:10:10 -0400

    First hooked up display to game state

--- public_html/js/dungeonroll.js
+++ public_html/js/dungeonroll.js
@@ -4,21 +4,21 @@ var whiteFaces = ["fighter", "mage", "cleric", "thief", "champion", "scroll"];
 var whiteImages = {fighter: "images/fighter.png", mage: "images/mage.png", cleric: "images/cleric.png", thief: "images/thief.png", champion:"images/champion.png", scroll: "images/scroll.png"};
 
 var WhiteDie = Backbone.Model.extend({
-	defaults: function() {
-		return {
-			face: "unknown"
-		};
-	},
-        
-	initialize: function() {
-		this.set({face: whiteFaces[Math.floor(Math.random() * whiteFaces.length)]});
-                this.set({graphic: whiteImages[this.get("face")]});
-	},
-	
-	reroll: function() {
-		this.set({face: whiteFaces[Math.floor(Math.random() * whiteFaces.length)]});
-                this.set({graphic: whiteImages[this.get("face")]});
-	},
+    defaults: function() {
+            return {
+                    face: "unknown"
+            };
+    },
+
+    initialize: function() {
+            this.set({face: whiteFaces[Math.floor(Math.random() * whiteFaces.length)]});
+            this.set({graphic: whiteImages[this.get("face")]});
+    },
+
+    reroll: function() {
+            this.set({face: whiteFaces[Math.floor(Math.random() * whiteFaces.length)]});
+            this.set({graphic: whiteImages[this.get("face")]});
+    },
 });
 
 var WhiteDieList = Backbone.Collection.extend({
@@ -28,66 +28,91 @@ var WhiteDieList = Backbone.Collection.extend({
 var Dice = new WhiteDieList;
 
 var WhiteDieView = Backbone.View.extend({
-	tagName: "span",
-	className: "die",
-	template: _.template($('#die-template').html()),
-	events: {
-	"click" : "dieClicked"
-	},
-	render: function() {
-		this.$el.html(this.template(this.model.toJSON()));
-		return this;
-	},
-	initialize: function() {
-		this.listenTo(this.model, 'change', this.render);
-		this.listenTo(this.model, 'destroy', this.remove);
-	},
-	
-	dieClicked: function() {
-		this.model.reroll();
-	}
+    tagName: "span",
+    className: "die",
+    template: _.template($('#die-template').html()),
+    events: {
+    "click" : "dieClicked"
+    },
+    render: function() {
+            this.$el.html(this.template(this.model.toJSON()));
+            return this;
+    },
+    initialize: function() {
+            this.listenTo(this.model, 'change', this.render);
+            this.listenTo(this.model, 'destroy', this.remove);
+    },
+
+    dieClicked: function() {
+            this.model.reroll();
+    }
 });
+
+var GameStateModel = Backbone.Model.extend({
+    defaults: function() {
+        return {
+            turn: 0,
+            phase: 1,
+            xp: 0,
+            level: 1
+        };
+    },
+    
+    advancePhase: function() {
+        this.set({phase: this.get("phase") + 1});
+    }
+});
+
+var GameState = new GameStateModel;
 
 var AppView = Backbone.View.extend({
 
-	el: $("#playArea"),
-	
+	el: $("#gameArea"),
+	phaseTemplate: _.template($('#phase-template').html()),
+        phaseDisplay: $("#phaseDisplay"),
+        
 	initialize: function() {
-		this.listenTo(Dice, 'add', this.addOne);
-		this.listenTo(Dice, 'remove', this.removeOne);
-		this.listenTo(Dice, 'all', this.render); //necessary?
-		
+            this.listenTo(Dice, 'add', this.addOne);
+            this.listenTo(Dice, 'remove', this.removeOne);
+            this.listenTo(Dice, 'all', this.render); //necessary?
 	},
 	
 	events: {
-		"click #add-button": "addDie",
-		"click #remove-button": "removeDie"
+            "click #add-button": "addDie",
+            "click #remove-button": "removeDie",
+            "click #advance-phase-button": "advancePhase",
 	},
 	
 	render: function() {
-		//nothing special here
+            this.phaseDisplay.html(this.phaseTemplate(GameState.toJSON()));
+
 	},
 	
 	addDie: function() {
-		Dice.create();
+            Dice.create();
 	},
 	
 	removeDie: function() {
-		Dice.remove(Dice.at(Dice.length - 1));
+            Dice.remove(Dice.at(Dice.length - 1));
 	},
 	
+        advancePhase: function() {
+            GameState.advancePhase();
+            this.render();
+        },
+        
 	addOne: function(die){
-		var dieView = new WhiteDieView({model: die});
-		this.$("#dice-area").append(dieView.render().el);
+            var dieView = new WhiteDieView({model: die});
+            this.$("#dice-area").append(dieView.render().el);
 	},
 	
 	addAll: function() {
-		Dice.each(this.addOne, this);
+            Dice.each(this.addOne, this);
     },
 	
 	removeOne: function(){
-		this.$("#dice-area").empty();
-		this.addAll();
+            this.$("#dice-area").empty();
+            this.addAll();
 	},
 	
 });
